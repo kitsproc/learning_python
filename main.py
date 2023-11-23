7cbe8db14548d0d1c9a2b25e3e2141dc00cdeebc
Amaury Merino <amerino>
2023-11-22 19:40:20 -0500

    Primera Leccion

---
+++ main.py
@@ -0,0 +1,22 @@
+#Hola mundo
+print("Hola Mundo")
+
+#Variables
+curso = 'Curso profesional de Python'
+print(curso)
+
+curso = 'Curso Python'
+print (curso)
+
+curso = 5
+print (curso)
+
+titulo_curso = 'Curso profesional de Python'
+print(titulo_curso)
+
+nombre_completo = 'Jose Gomez'
+print(nombre_completo)
+
+#constantes
+TITULOS_CURSOS = 'Curso profesional de Python' 
+print(TITULOS_CURSOS)
