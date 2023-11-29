53b5848762244f34c5828254cba72a8d22326b6d
Amaury Merino <amerino>
2023-11-29 18:25:12 -0500

    temas interesantes de listas

---
+++ Modulo2/index.py
@@ -0,0 +1,5 @@
+lista = [8,1,5,7,8,23]
+print(lista)
+print(5 in lista)
+
+print("El indice del elemento 5 es: ",lista.index(5))
---
+++ Modulo2/matrices.py
@@ -0,0 +1,13 @@
+#Matriz de 2 x 2
+columna_a = [10, 20]
+columna_b = [30, 40]
+matriz = [columna_a, columna_b] # Matriz de 2 x 2
+print(matriz) 
+
+#Matriz de 3 x 3 
+columna1_a = [10, 20, 30]
+columna2_b = [31, 40, 57]
+columna3_c = [10, 20, 89]
+
+matriz3x3 = [columna1_a, columna2_b, columna3_c] # Matriz de 2 x 2
+print(matriz3x3)
